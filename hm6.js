b5205a019d8479ae94ece0467349d9d9dc443df6
Ihor Kalashnyk <allo011016allo@gmail.com>
2023-07-12 20:00:16 -0500

    homework7

--- hm6.js
+++ hm6.js
@@ -10,6 +10,26 @@
 // }
 
 
+//test
+// const number = maxNumber(60, 65);
+// console.log(number);
+
+// function maxNumber(number1, number2){
+//     if (number1 > number2) {
+//         console.log("false")
+//         return number1;
+//     } else {
+//         console.log('true')
+//         return number2;
+//     }
+        
+        
+    
+    
+// }
+
+
+
 
 
 // let amount = maxNumber(100, 500);
@@ -45,6 +65,15 @@
 // }
 // }
 
+//test arowFunction homework7
+
+// const maxNumber = (a, b) => a > b;
+// const result = maxNumber(150, 1000);
+// console.log(result)
+
+// const maxNumber = (a, b) => {return a < b};
+// const result = maxNumber(100, 1000);
+// console.log(result);
 
 //2. Написати функцію checkAge(age), яка отримую вік користувача і якщо йому меньше 18, тоді повертати false, інакше - true
 
@@ -60,11 +89,32 @@
 // console.log(result)
 
 // const result2 = checkAge(20);
-// console.log(result2)
+// console.log(result2);
+
+//test homeework arrowfunction
+
+// const checkAge = (age) => age < 18;
+// const result = checkAge(17);
+// console.log(result);
+
+// const checkAge1 = (age) => age < 18;
+// const result1 = checkAge1(20);
+// console.log(result1);
 
 
+// const checkAge = (age) => age < 18;
+// const result = checkAge(17);
+// console.log(result);
 
-// Переписати  “ДЗ 4. Усічення рядка” на окрему функцію.
+// const checkAge = (age) => {return age > 18};
+// const result = checkAge(17);
+// console.log(result)
+
+// const checkAge = (age) => {return age < 45};
+// const result = checkAge(100);
+// console.log(result);
+
+// 3. Переписати  “ДЗ 4. Усічення рядка” на окрему функцію.
 
 // Можете залишити prompt, або отримувати параметри, як аргумент.
 
@@ -106,9 +156,6 @@
 //     }
 // }
 
-
-
-
 // const result = cutString(str, maxLength);
 // console.log(result);
 
@@ -116,37 +163,108 @@
 //console.log(result4);
 
 
- 
 
+  //test homework7 arrowfunction
+
+//   const cutString = (str, maxLength) => {
+//     if (str.length > maxLength) { 
+//         return str.slice(0, maxLength) +'...';
+//     } else {
+//         return str;
+//     }
+    
+
+// }
+// console.log(cutString("Яка ваша максимальна довжина рядка?", 25)); 
+
+// const cutString = (str, maxLength) =>{
+//     if (str.length > maxLength) {
+//         return str.slice(0, maxLength) +"..."
+//     } else {
+//         return str;
+//     }
+// }
+//  console.log(cutString("Яка Ваша довільна довжина рядка?", 25));
+
+// const cutString = (str, maxLength) =>{
+//     if (str.length > maxLength) {
+//         return str.slice(0, maxLength) +"...";
+//     } else {
+//         return str;
+//     }
+// }
+// console.log(cutString("What are you doing write now?", 25));
+
+// const cutString = (str, maxLength) =>{
+//     if (str.length > maxLength){
+//         return str.slice(0, maxLength) +"...";
+//     } else {
+//         return str;
+//     }
+// }
+// console.log(cutString("gtgrtttttnthnhnhnhnhnhtnhtt", 25));
+
+// const cutString = (str, maxLength) =>{
+//     if (str.length > maxLength) {
+//         return str.slice(0, maxLength) +"..grgthgh";
+//     } else {
+//         return str;
+//     }
+// }
+//   console.log(cutString('ergghhrhththtyytytttmmm', 15));
 // 4. Напишіть функцію checkWord(str, word), яка приймає рядок str і слово word.
 
 // Функція має повернути true, якщо слово знаходиться у рядку, інакше повернути false.
 
-const str = "How many letters are there in that word?"
+// const str = "How many letters are there in that word?"
 
-function checkWord (str){
-    if (str.includes("word")){
+// function checkWord (str){
+//     if (str.includes("word")){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// function checkWord1 (str){
+//     if (str.includes("repeat")){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// const result = checkWord(str)
+// console.log(result)
+
+// const result1 = checkWord1(str)
+// console.log(result1)
+
+const checkWord = (str) =>{
+    if (str.includes('word')){
         return true;
     } else {
         return false;
     }
 }
-function checkWord1 (str){
-    if (str.includes("repeat")){
-        return true;
-    } else {
-        return false;
-    }
-}
-
-const result = checkWord(str)
+const result = checkWord('How many letters are there in that word?');
 console.log(result)
+//another way
+console.log(checkWord("How many letters are there in that word?"));
+//Test 
 
-const result1 = checkWord1(str)
-console.log(result1)
+// const str = "How many words are there in the boock?"
+// const maxLength = "15"
+
+// function cutString(str, maxLength){
+//     if (str.length > maxLength) {
+//         return str.slice(0, maxLength) +"...";
+//     } else {
+//         return str;
+//     }
 
 
-
-
+// }
+// const result = cutString(str, maxLength);
+// console.log(result)
 
 
