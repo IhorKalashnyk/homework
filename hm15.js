ffae450ea36ca5051cfc07665bc1a1c890b6473a
Ihor Kalashnyk <allo011016allo@gmail.com>
2023-08-07 14:07:31 -0500

    hm15

---
+++ hm15.js
@@ -0,0 +1,15 @@
+
+
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i)
+}
+
+
+
+
+
+// а твкий варіант не підійде?
+// const number = [2, 4, 6, 8, 10]
+// for (let num of number) {
+//     console.log (num);
+// }
