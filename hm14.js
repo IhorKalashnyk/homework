d4f958ee3533c08b1873f03fd9a9cde1d0e53242
Ihor Kalashnyk <allo011016allo@gmail.com>
2023-08-07 18:32:29 -0500

    рщьуцщкл14

---
+++ hm14.js
@@ -0,0 +1,16 @@
+const number = +prompt('Введіть число між 0 та 3', '');
+
+ switch (number) {     
+ case 0:
+    case 1:
+        case 2:
+            case 3: 
+            alert('ви ввели від 0 до 3')
+ console.log('ви ввели від 0 до 3' )
+ break;     
+ default:
+    alert('ви ввели неправильну цифру')
+    console.log('ви ввели неправильну цифру')
+break;
+ }
+
