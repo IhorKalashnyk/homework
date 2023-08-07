0d781add5c2d1a2d27cec22b52530fe5ad9f817a
Ihor Kalashnyk <allo011016allo@gmail.com>
2023-08-07 14:23:13 -0500

    rebuild

--- hm10.js
+++ hm10.js
@@ -22,6 +22,170 @@ const numbersArray = [1, 2, 4, 7, 10]
 
 
 
+
+// const numberArray = [1, 2, 4, 7, 10];
+// function findAverage (number) {
+//     if (number.length === 0)
+//     return 0;
+
+
+//     const amountPluss = number.reduce((acc, number) => acc + number)
+//     return amountPluss / number.length
+
+// }
+
+
+// const result = findAverage(numberArray)
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numbersArray = [1, 2, 4, 7, 10];
+// function findAverage(numbers) {
+//     if (numbers.length === 0)
+//     return 0;
+
+//     const amountPlus = numbers.map((acc, numbers)=> acc + numbers, 0)
+//     return amountPlus / numbers;
+// }
+
+ 
+// const result = (findAvresult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  const numbersArray = [1, 2, 3, 4, 7, 10];
+//  function findAverage(numbers) {
+//     if (numbers.length === 0)
+//     return 0;
+
+
+//     const amountPluss = numbers.map((acc, numbers)=> acc + numbers, 0)
+//     return amountPluss / numbers.length
+//  }
+
+// const result = findAverage(numbersArray);
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numbersArray = [1, 2, 3, 4, 7, 10];
+// function findAverage(numbers) {
+//     if (numbers.length === 0)
+//     return 0;
+
+
+//     const amountPlus = numbers.reduce((acc, numbers)=> acc + numbers, 0)
+//     return amountPlus / numbers.length
+// }
+// const result = findAverage(numbersArray);
+// console.log(result);
+
+
+// const numbersArray = [1, 2, 3, 4, 7, 10];
+
+// function findAverage (numbers) {
+//     if (numbers.length === 0)
+//     return 0
+// }
+
+// const amountPluss = numbers.reduce((acc, numbers)=> acc + numbers, 0);
+// return amountPluss / numbers.length 
+
+// const result = findAverage(numbersArray);
+// consolr.log(result);
+// ;
+
+
+
+// const numbersArray = [1, 2, 3, 4, 7, 10]
+// function findAverage(numbers) {
+//     if (numbers.length ===0) {
+//         return 0;
+//     }
+
+//     const amountPlus = numbers.reduce((acc, numbers)=> acc + numbers, 0);
+
+//     return amountPlus / numbers.length
+
+// }
+
+// const result = findAverage(numbersArray);
+// console.log(result);
+
+
+
+
+
+
 //   const numbersArray = []
 
 //  function findAverage(numbers) {
---
+++ hm12.js
@@ -0,0 +1,202 @@
+// const addStrings = (num1, num2) => {
+//     return (+111 + +2222).toString();
+// };
+// const result = addStrings("111", "2222");
+// console.log(result);
+
+
+
+
+
+// let i = 0;
+// do {
+//     console.log(i)
+// i ++
+// } while (i > 10) 
+   
+
+
+// for (let i = 0; 1 < 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// const emploees = ['lara', 'sukhi', 'evee', 'simi', 'beno', 'jay'];
+// const emploeesSalary = [100, 1300, 957.89, 3230,14, 750, 13900]
+
+// for(let i = 0; i < emploees.length; i++) {
+//     console.log(`${emploees[i]}: ${emploeesSalary[i]}`)
+// }
+
+// const emploees = ['lara', 'sukhi', 'evee', 'simi', 'beno', 'jay'];
+// const emploeesSalary = [100, 1300, 957.89, 3230,14, 750, 13900]
+// const obj = {}
+// for(let i = 0; i < emploees.length; i++) {
+// }
+// console.log(obj)
+
+
+
+
+//  const findindex = (arr, value) =>
+//  for (let i = 0; i < arr.length; i++) {
+//      if(arr[i] === value) return i;
+//  }
+//  return -1
+
+//  const arr = [1, 2, 7, 4]
+//  console.log(findindex(arr, 4));
+//  console.log(findindex(arr, 5));
+
+
+
+
+// const objectA = {
+//     a: 10,
+//     b: true
+// }
+// const copyOfA = objectA
+
+// copyOfA.c = 20
+
+// copyOfA.d = 30
+
+
+
+// console.log(objectA)
+
+// const a = () => {
+//     console.log('Hey There')
+// }
+
+// a()
+
+const myCity = {
+city: 'New York',
+popular: true,
+coutry: 'USA'
+}
+myCity.popular = false
+console.log(myCity.popular)
+
+myCity.weather = 'heat'
+console.log(myCity)
+delete myCity.popular
+console.log(myCity)
+myCity['perfect'] = 'yes'
+console.log(myCity)
+const spaciousPlace = 'do you like it?'
+myCity [spaciousPlace] = 'I like it'
+console.log(myCity);
+
+
+
+
+
+
+
+
+
+const myHome = {
+    stylish: true,
+    street: 'Volkova',
+    country: 'USA'
+}
+myHome.street = '9501 NORTH 620 FM'
+console.log(myHome.street)
+
+myHome.comfortable = 'yes'
+console.log(myHome)
+delete myHome.street
+console.log(myHome)
+myHome['modern'] = true
+console.log(myHome)
+
+const goodPlace = 'is it Nice?';
+myHome [goodPlace] = 'yes'
+console.log(myHome);
+
+
+
+const myLessons = {
+    online: true,
+    atThestreet: '620FM',
+    laptop: 'hp'
+}
+myLessons.atThestreet = '9501 North 620FM'
+console.log(myLessons.atThestreet)
+
+myLessons.like = 'false'
+console.log(myLessons)
+
+delete myLessons.atThestreet
+console.log(myLessons)
+myLessons['course'] = 'paid'
+console.log(myLessons)
+
+const borderOfKnowlege = 'progress';
+myLessons [borderOfKnowlege] = 'keep going'
+
+
+
+
+
+
+
+
+
+
+
+const myLove = {
+    wife: 'true',
+    want: 'sex',
+    beautiful: 'yes'
+}
+myLove.want = 'love'
+console.log(myLove.want)
+
+myLove.like ='gold'
+console.log(myLove)
+
+delete myLove.want
+console.log(myLove)
+
+myLove['love'] ='dollars'
+console.log(myLove)
+
+const valueOfLove = 'wayOfLove';
+myLove[valueOfLove] = true
+
+console.log(myLove)
+
+
+
+
+
+
+
+
+
+const Studing = {
+    difficult: true,
+    acomedationWith: 'laptop',
+    use: 'typing'
+}
+Studing.difficult = false
+console.log(Studing.difficult)
+
+Studing.city = 'Austin'
+Studing.where = 'at home'
+console.log(Studing)
+
+
+delete Studing.where
+console.log(Studing)
+
+Studing['course'] = 'front end developer' 
+console.log(Studing)
+
+const wayOfStudind = 'typeOfStuding'
+Studing [wayOfStudind] = 'remotely'
+console.log(Studing)
