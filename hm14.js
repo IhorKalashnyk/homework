81a045ff08183045e25cf7d1b9f4cdaece2bdf17
Ihor Kalashnyk <allo011016allo@gmail.com>
2023-08-07 19:08:38 -0500

    rebuild

--- hm14.js
+++ hm14.js
@@ -2,15 +2,20 @@ const number = +prompt('Введіть число між 0 та 3', '');
 
  switch (number) {     
  case 0:
+    alert('ви ввели число 0')
+    console.log('ви ввели число 0 ')
+    break;
     case 1:
-        case 2:
-            case 3: 
-            alert('ви ввели від 0 до 3')
- console.log('ви ввели від 0 до 3' )
- break;     
- default:
-    alert('ви ввели неправильну цифру')
-    console.log('ви ввели неправильну цифру')
-break;
- }
-
+    alert('Ви ввели число 1')
+    console.log('ви ввели число 1')
+    break;
+    case 2:
+    case 3:
+    alert('Ви ввели число 2 або 3')
+    console.log('ви ввели число 2 або 3')
+    break;
+    default:
+    alert('ви ввели число більше 3')
+    console.log('ви ввели число більше 3')
+    break;
+}    
